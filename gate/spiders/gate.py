6fba9cb252da3bb763907d5448deb6e523ed484a
Mohammed Rifas S <rifas2475@outlook.com>
2020-07-11 18:37:18 +0530

    Gate Spider Added
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

---
+++ gate/spiders/gate.py
@@ -0,0 +1,30 @@
+import scrapy
+import re
+
+
+class GateSpider(scrapy.Spider):
+    name = 'gate'
+    allowed_domains = ['questions.examside.com']
+    start_urls = [
+        "https://questions.examside.com",
+    ]
+    interesting_url = re.compile("https://questions.examside.com"
+                                 "/past-years/gate/"
+                                 "question/[\\w-]+.htm")
+
+    links = []
+
+    def parse(self, response):
+        anchor = response.css("a::attr(href)").getall()
+        for link in anchor:
+            link = self.start_urls[0] + link
+            if re.match(self.interesting_url, str(link)):
+                print(">>> Link found!\n\tAppending")
+                self.links.append(link)
+
+        for next_page in anchor:
+            yield response.follow(self.start_urls[0] + next_page, self.parse)
+
+
+# TODO: Implement Callback  
+#  Reference https://github.com/scrapy/booksbot/blob/master/books/spiders/books.py
