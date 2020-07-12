51f156c3a8a5ee279585694415fbc547c0e668ea
Mohammed Rifas S <rifas2475@outlook.com>
2020-07-12 15:37:19 +0530

    Selenium Implementation
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- gate/spiders/gate_selenium.py
+++ gate/spiders/gate_selenium.py
@@ -12,14 +12,18 @@ class GateSelenium:
         self.driver.get(question_url)
         try:
             meta = self.driver.find_element_by_css_selector("div.question-body > h3").text
-            question = self.driver.find_element_by_css_selector("div.question-body > div > p").text
+            question = self.driver.find_element_by_css_selector("div.question-body > div").get_attribute("innerHTML")
             diagram = self.driver.find_element_by_css_selector("div.question-body > div > img").get_attribute("src")
-            answer = self.driver.find_element_by_xpath("div.question-solution-container > div.pa-8.text-center")
-            options = self.driver.find_elements_by_css_selector("div.question-body > div.question-options")
+
+            self.driver.find_element_by_css_selector("div.question-actions.text-center > button").click()
+
+            answer = self.driver.find_element_by_css_selector("div.pa-8.text-center>b").text
+            options = self.driver.find_elements_by_css_selector("div.option-item.flex.ripple.flex-middle-xs>div.pa-4>span.mjx-chtml MathJax_CHTML")
+            options = [option.get_attribute("innerHTML") for option in options]
 
             if answer is None:
-                options = None
-                self.driver.find_element_by_css_selector("div.question-actions.text-center > button").click()
+                # options = None
+
                 answer = self.driver.find_element_by_css_selector("div.overlay.correct").parent
 
             scraped_data = {
@@ -39,12 +43,19 @@ class GateSelenium:
 if __name__ == "__main__":
     gate_instance = GateSelenium()
 
-    url = "https://questions.examside.com/" \
-          "past-years/gate/question/" \
-          "a-connection-is-made-consisting-of-resistance-a" \
-          "-in-series-wi-gate-ece-2017-set-2-" \
-          "marks-1-5a00e7c2d6ba4.htm"
+    no_option_url = "https://questions.examside.com/" \
+                    "past-years/gate/question/" \
+                    "a-connection-is-made-consisting-of-resistance-a" \
+                    "-in-series-wi-gate-ece-2017-set-2-" \
+                    "marks-1-5a00e7c2d6ba4.htm"
 
-    data = gate_instance.scrape_question(question_url=url)
+    option_url = "https://questions.examside.com/" \
+                 "past-years/gate/question/" \
+                 "in-the-circuit-shown-below-vs-is-a-constant-voltage-" \
+                 "source-a-gate-ece-2016-set-2-marks-1" \
+                 "-5a05775344abf.htm"
+
+    # data = gate_instance.scrape_question(question_url=no_option_url)
+    data = gate_instance.scrape_question(question_url=option_url)
 
     print(data)
