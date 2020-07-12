2a84c386d33ecf39ce3404a0aef0d5860e8a119e
Mohammed Rifas S <rifas2475@outlook.com>
2020-07-12 17:29:22 +0530

    Selenium Implementation Complete
    Signed-off-by: Mohammed Rifas S <rifas2475@outlook.com>

--- gate/spiders/gate_selenium.py
+++ gate/spiders/gate_selenium.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 
@@ -8,35 +10,103 @@ class GateSelenium:
     def __init__(self):
         self.driver = webdriver.Chrome("chromedriver.exe")
 
+    @staticmethod
+    def print_url(question_url):
+        print("\tURL:", question_url)
+
     def scrape_question(self, question_url):
         self.driver.get(question_url)
+
+        meta = None
+        question = None
+        diagrams = None
+        options = None
+        answer = None
+
         try:
             meta = self.driver.find_element_by_css_selector("div.question-body > h3").text
-            question = self.driver.find_element_by_css_selector("div.question-body > div").get_attribute("innerHTML")
-            diagram = self.driver.find_element_by_css_selector("div.question-body > div > img").get_attribute("src")
-
-            self.driver.find_element_by_css_selector("div.question-actions.text-center > button").click()
-
-            answer = self.driver.find_element_by_css_selector("div.pa-8.text-center>b").text
-            options = self.driver.find_elements_by_css_selector("div.option-item.flex.ripple.flex-middle-xs>div.pa-4>span.mjx-chtml.MathJax_CHTML")
-            options = [option.get_attribute("innerHTML") for option in options]
-
-            if answer is "":
-                answer = self.driver.find_element_by_css_selector(
-                    "div.overlay.correct").find_element_by_xpath("..").find_element_by_css_selector("div.ma-4.flex.flex-center-xs.flex-middle-xs.primary-color-bg.green-500-bg").text
-
-            scraped_data = {
-                "question_meta": meta,
-                "question": question,
-                "diagram": diagram,
-                "options": options,
-                "answer": answer
-            }
-
-            return scraped_data
-
         except Exception as e:
-            print(e)
+            print("Meta Not Found", e)
+            self.print_url(question_url)
+            meta = None
+
+        try:
+            question = self.driver.find_element_by_css_selector("div.question-body > div").get_attribute("innerHTML")
+        except Exception as e:
+            print("WARNING!! Question not Scraped", e)
+            self.print_url(question_url)
+            exit(0)
+
+        try:
+            diagram = self.driver.find_element_by_css_selector("div.question-body > div > img").get_attribute("src")
+        except Exception as e:
+            print("Diagrams Not Found", e)
+            self.print_url(question_url)
+            diagram = None
+
+        try:
+            self.driver.find_element_by_css_selector("div.question-actions.text-center > button").click()
+        except Exception as e:
+            print("WARNING!! Button Could not be clicked", e)
+            self.print_url(question_url)
+            exit(0)
+
+        time.sleep(2)
+
+        try:
+            answer = self.driver.find_element_by_css_selector("div.pa-8.text-center>b").text
+            print("Subjective Answer Found")
+            if answer is "":
+                print("Answer is Objective")
+                raise Exception
+        except Exception as e:
+            try:
+                print("Subjective Answer Not found", e)
+                answer = self.driver.find_element_by_css_selector(
+                    "div.overlay.correct").find_element_by_xpath("..").find_element_by_css_selector(
+                    "div.ma-4.flex.flex-center-xs.flex-middle-xs.primary-color-bg.green-500-bg").text
+                print("Objective Answer Found")
+            except Exception as e:
+                print("WARNING!! Answer Could not be Obtained", e)
+                self.print_url(question_url)
+                exit(0)
+
+        try:
+            options = self.driver.find_elements_by_css_selector("div.option-item.flex.ripple.flex-middle-xs>div.pa-4>span.mjx-chtml.MathJax_CHTML")
+            if len(options) == 0:
+                raise Exception
+            options = [option.get_attribute("innerHTML") for option in options]
+            print("MathXML Options Found")
+        except Exception as e:
+            try:
+                options = self.driver.find_elements_by_css_selector(
+                    "div.option-item.flex.ripple.flex-middle-xs>div.pa-4>img")
+                options = [option.get_attribute("src") for option in options]
+                print("No MathXML options found", e)
+                print("Obtained Image options")
+                self.print_url(question_url)
+            except Exception as e:
+                try:
+                    options = self.driver.find_elements_by_css_selector(
+                        "div.option-item.flex.ripple.flex-middle-xs>div.pa-4")
+                    options = [option.text for option in options]
+                    print("No MathXML or Image Options Found", e)
+                    print("Obtained Textual options")
+                    self.print_url(question_url)
+                except Exception as e:
+                    options = []
+                    print("WARNING!! No options found", e)
+                    self.print_url(question_url)
+
+        scraped_data = {
+            "question_meta": meta,
+            "question": question,
+            "diagram": diagram,
+            "options": options,
+            "answer": answer
+        }
+
+        return scraped_data
 
 
 if __name__ == "__main__":
@@ -54,10 +124,29 @@ if __name__ == "__main__":
                  "source-a-gate-ece-2016-set-2-marks-1" \
                  "-5a05775344abf.htm"
 
+    image_option_url = "https://questions.examside.com/" \
+                       "past-years/gate/question/" \
+                       "which-one-of-the-following-diagrams-shows-correctly" \
+                       "-the-dist-gate-me-1990-marks-1-1oig1ef8ehgstiub.htm"
+
+    text_option_url = "https://questions.examside.com/" \
+                      "past-years/gate/question/" \
+                      "consider-the-function-func-shown-belowint-funcint-" \
+                      "num-nbspnb-gate-cse-2014-set-2-marks-1" \
+                      "-jzahjwa4rvhekjvm.htm"
+
     print("Selenium on question with no Options")
     data = gate_instance.scrape_question(question_url=no_option_url)
     print(data)
 
     print("Selenium on question with Options")
     data = gate_instance.scrape_question(question_url=option_url)
+    print(data)
+
+    print("Selenium on question with Image Options")
+    data = gate_instance.scrape_question(question_url=image_option_url)
+    print(data)
+
+    print("Selenium on question with Text Options")
+    data = gate_instance.scrape_question(question_url=text_option_url)
     print(data)
